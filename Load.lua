694051af18bfd897fda97cd7aaa84b1bbb3ec945
Souls <94298035+Entoy77@users.noreply.github.com>
2021-11-22 16:36:44 -0300

    Update Load.lua

--- Load.lua
+++ Load.lua
@@ -1,7 +1,7 @@
 local MarketplaceService = game:GetService("MarketplaceService")
 
 local games = {
-    [402122991] = {src=""}, -- Blox Fruits
+    [155615604] = {src="https://raw.githubusercontent.com/Entoy77/Dark-Hub/main/Init"}, -- Blox Fruits
 } 
 
 local check = games[game.PlaceId] 
@@ -10,5 +10,5 @@ if check then
     loadstring(game:HttpGet(check.src, true))()
 else
     local Players = game:GetService("Players")
-    Players.LocalPlayer:Kick(string.format("%s is unsupported. Just for Prison life btw.", MarketplaceService:GetProductInfo(game.PlaceId).Name))
+    Players.LocalPlayer:Kick(string.format("%s is unsupported. The Hub Is Just for Prison life btw.", MarketplaceService:GetProductInfo(game.PlaceId).Name))
 end
