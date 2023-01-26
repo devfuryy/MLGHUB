d1124fba6567a970222f554d42d702fdf38e9116
♻️ <122626171+devfuryy@users.noreply.github.com>
2023-01-26 15:02:43 -0300

    Update load.lua

--- load.lua
+++ load.lua
@@ -1,7 +1,7 @@
 local MarketplaceService = game:GetService("MarketplaceService")
 
 local games = {
-    [155615604] = {src="https://raw.githubusercontent.com/Entoy77/MLGHUB/main/PrisonLife.lua"}, -- Only Works On Prison Life
+    [155615604] = {src="https://raw.githubusercontent.com/devfuryy/MLGHUB/main/PrisonLife.lua"}, -- Only Works On Prison Life
 } 
 
 local check = games[game.PlaceId] 
@@ -10,5 +10,5 @@ if check then
     loadstring(game:HttpGet(check.src, true))()
 else
     local Players = game:GetService("Players")
-    Players.LocalPlayer:Kick(string.format("%s is unsupported. The Hub Is Just for Prison life btw.", MarketplaceService:GetProductInfo(game.PlaceId).Name))
+    Players.LocalPlayer:Kick(string.format("%s is unsupported. The MLGHUB Just Works for Prison life btw.", MarketplaceService:GetProductInfo(game.PlaceId).Name))
 end
